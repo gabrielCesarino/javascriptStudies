e93eef7f61f03af85e0d99664e69087be982042a
gabrielCesarino <gahnrq@hotmail.com>
2020-07-21 21:07:51 -0300

    DOM exercises

--- DOM/DOM-inicio/script.js
+++ DOM/DOM-inicio/script.js
@@ -3,3 +3,26 @@ const href = location.href//window é global por tanto não precisa ser chamado 
 const h1Text = document.querySelector('h1')//document faz referência ao HTML
 
 console.log(h1Text.innerHTML)
+
+//addEventListener
+
+document.querySelector('button').addEventListener('click', function(){
+    console.log('botão')
+})
+
+//Exercicios
+
+//Retorne o url da página atual utilizando o objeto window
+
+console.log(location.href)
+
+//Seleciona o primeiro elemento da página que
+//possua a classe ativo
+
+document.querySelector('.ativo')
+
+//Retorne a linguagem do navegador
+console.log(window.navigator.language)
+
+//Retorne o tamanho da janela
+console.log(window.innerWidth)
