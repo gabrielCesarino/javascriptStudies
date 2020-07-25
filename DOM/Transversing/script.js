52911f765d99ba65bdaf14bcdb81215a738d5525
gabrielCesarino <gahnrq@hotmail.com>
2020-07-25 15:32:31 -0300

    Manipulando elementos

--- DOM/Transversing/script.js
+++ DOM/Transversing/script.js
@@ -23,3 +23,43 @@ console.log(ul.querySelector('li:last-child'))
 //Nodes
 
 console.log(h1.nextSibling)
+
+//Maniupulando elementos
+
+ul.appendChild(h1)//Adiciona ao último filho 
+console.log(ul)
+
+const primeiroLink = document.querySelector('li')
+
+ul.insertBefore(h1, primeiroLink)
+
+console.log(ul)
+
+//Criando elementos
+
+const novoH2 = document.createElement('h2')
+novoH2.innerText = 'Novo titulo'
+
+ul.appendChild(novoH2)
+
+const novoH1 = h1.cloneNode(true)
+
+console.log(novoH1)
+
+
+//Exercicio
+
+//Duplique o menu e adicione ele em copy
+
+const novoUl = ul.cloneNode(true)
+
+ul.appendChild(novoUl)
+
+//Selecione o segundo link da lista
+
+const segundoLink = primeiroLink.nextElementSibling
+console.log(segundoLink)
+
+//Substitua o primeiro titulo pelo segundo
+
+h1.innerHTML = novoH2.innerHTML
