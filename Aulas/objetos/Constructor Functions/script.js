c026fc61b739b2b33a65d9c48654e8b1d68214d4
gabrielCesarino <gahnrq@hotmail.com>
2020-07-26 20:51:15 -0300

    constructionFunction 1

---
+++ Aulas/objetos/Constructor Functions/script.js
@@ -0,0 +1,12 @@
+//Construction function
+
+function Carro(marca, precoInicial){
+    const taxa = 1.2
+    const precoFinal = precoInicial * taxa
+    this.marca = marca
+    this.preco = precoFinal
+}
+
+const honda = new Carro('Honda', 4000)
+const fiat = new Carro('Fiat', 2000)
+
