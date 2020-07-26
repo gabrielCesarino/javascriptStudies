5d36cb72abf9cce2349e2c677c806fdc1fc1ab39
gabrielCesarino <gahnrq@hotmail.com>
2020-07-25 21:46:40 -0300

    add: smoothScroll

--- Projeto - Animais Fantásticos/script.js
+++ Projeto - Animais Fantásticos/script.js
@@ -41,3 +41,32 @@ accordionList.forEach((item) => {
 }
 
 initAccordion()
+
+function initSmoothScroll(){
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"')
+
+function scrollToSection(e){
+    e.preventDefault()
+    const href = e.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
+    const topo = section.offsetTop
+
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
+    //Forma alternativa
+    // window.scrollTo({
+    //     top: topo,
+    //     behavior: 'smooth'
+    // })
+}
+
+linksInternos.forEach((link) =>{
+    link.addEventListener('click', scrollToSection)
+})
+
+}
+
+initSmoothScroll()
