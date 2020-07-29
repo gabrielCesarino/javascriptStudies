3973e1605410b72c065fa725e615d0cf61acf8f1
gabrielCesarino <gahnrq@hotmail.com>
2020-07-28 22:48:16 -0300

    prototype 2

--- Aulas/objetos/Prototype/script.js
+++ Aulas/objetos/Prototype/script.js
@@ -1,14 +1,60 @@
-function Pessoa (nome, idade){
+// function Pessoa (nome, idade){
+//     this.nome = nome
+//     this.idade = idade
+// }
+
+
+// const andre = new Pessoa('Andre', 28)
+
+// console.log(Pessoa.prototype)
+// console.log(andre.prototype)
+
+// Pessoa.prototype.andar  = function(){
+//     return 'Pessoa andou'
+// }
+
+const pais = 'Brasil'
+const cidade = new String ('Rio')
+
+const listAnimais = ['Cachorro', 'Gato', 'Cavalo']
+
+const listaArray = document.querySelectorAll('li')
+
+console.log(Array.prototype.slice.call(listaArray))
+console.log(Array.from(listaArray))
+
+
+const Carro = {
+    marca: 'Ford',
+    preco: 2000,
+    andar() {
+        return true
+    }
+}
+
+//Exercicios
+
+//Crie uma função construtora de Pessoas
+//Deve conter nome, sobrenome e idade
+//Crie um método no prototipo que retorne
+//o nome completo da pessoa
+
+function Pessoa(nome, sobrenome, idade){
     this.nome = nome
+    this.sobrenome = sobrenome
     this.idade = idade
 }
 
+Pessoa.prototype.nomeCompleto = function () {
+        return `${this.nome} ${this.sobrenome}`
+    }
 
-const andre = new Pessoa('Andre', 28)
+const p1 = new Pessoa ('João', 'Henrique', '18')
 
-console.log(Pessoa.prototype)
-console.log(andre.prototype)
+//Liste os métodos acessados por 
+//dados criados com NodeList
+//HTMLCollection, Document
 
-Pessoa.prototype.andar  = function(){
-    return 'Pessoa andou'
-}
+console.log(Object.getOwnPropertyNames(NodeList.prototype))
+console.log(Object.getOwnPropertyNames(HTMLCollection.prototype))
+console.log(Object.getOwnPropertyNames(Document.prototype))
