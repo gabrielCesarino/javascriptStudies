98a72fc808a676a83667f3ae84d2578ba3c7b1b1
gabrielCesarino <gahnrq@hotmail.com>
2020-07-29 22:03:12 -0300

    differnt objects type

---
+++ Aulas/objetos/Differents types/script.js
@@ -0,0 +1,20 @@
+//Liste 5 objetos nativos
+
+console.log(Object)
+console.log(Function)
+console.log(Boolean)
+console.log(String)
+console.log(Number)
+
+//List 5 objetos do browser
+
+console.log(addEventListener)
+console.log(window)
+console.log(Document)
+console.log(NodeList)
+console.log(HTMLCollection)
+
+//Liste 2 métodos, propriedades ou Objetos presentes no Chrome que não existem no Firefox
+
+console.log(typeof document.webkitVisibilityState)
+console.log(typeof document.webkitHidden)
