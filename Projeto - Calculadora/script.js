246bc4f54ebb39f53c13919ed46e363d4ecdd788
gabrielCesarino <gahnrq@hotmail.com>
2020-08-11 19:07:03 -0300

    add: showing values on screen

--- Projeto - Calculadora/script.js
+++ Projeto - Calculadora/script.js
@@ -1,3 +1,17 @@
-function calc(tipo, valor) {
-    console.log(tipo, valor);
-}
+
+const result = document.querySelector('#result');
+
+function calc(type, value) {
+    
+    if(type === 'action'){
+        if(value === 'c'){
+            result.value = ' ';
+        }else if (value === '='){
+            
+        }else {
+            result.value += value;
+        }
+    }else if( type === 'value'){
+        result.value += value;
+    };
+};
