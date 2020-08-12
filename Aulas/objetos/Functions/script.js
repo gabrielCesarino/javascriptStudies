3f29f0048383e42b9d1a7cdc8163932573a2c4be
gabrielCesarino <gahnrq@hotmail.com>
2020-08-11 21:41:52 -0300

    function.bind()

--- Aulas/objetos/Functions/script.js
+++ Aulas/objetos/Functions/script.js
@@ -45,3 +45,14 @@ const numeros = [20, 23, 25, 2];
 
 console.log(Math.max.apply(null, numeros));
 
+//Argumentos comuns com bind
+
+function imc(altura, peso) {
+    return peso / (altura * altura )
+}
+
+const imc180 = imc.bind(null, 1.80)
+
+imc(1.88, 70);
+
+imc(70)
