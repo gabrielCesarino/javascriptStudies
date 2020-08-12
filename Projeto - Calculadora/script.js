39f4c4e741ea2f9efa7d101e4138f6d8e9886f8b
gabrielCesarino <gahnrq@hotmail.com>
2020-08-12 07:52:40 -0300

    add:forEach to run all Items

--- Projeto - Calculadora/script.js
+++ Projeto - Calculadora/script.js
@@ -1,17 +1,29 @@
 
+// const result = document.querySelector('#result');
+
+// function calc(type, value) {
+//     if(type === 'action'){
+//         if(value === 'c'){
+//             result.value = ' ';
+//         }else if (value === '='){
+//             const valorCamp = result.value;
+//             result.value = eval(valorCamp);
+//         }else {
+//             result.value += value;
+//         }
+//     }else if(type === 'value'){
+//         result.value += value;
+//     };
+// };
 const result = document.querySelector('#result');
 
-function calc(type, value) {
-    if(type === 'action'){
-        if(value === 'c'){
-            result.value = ' ';
-        }else if (value === '='){
-            const valorCamp = result.value;
-            result.value = eval(valorCamp);
-        }else {
-            result.value += value;
-        }
-    }else if(type === 'value'){
-        result.value += value;
-    };
-};
+const allButtons = document.querySelectorAll('button');
+
+console.log(allButtons);
+
+allButtons.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        if(e.target.textContent !== 'C' && e.target.textContent !== '=')
+        result.value += e.target.textContent;
+    })
+})
