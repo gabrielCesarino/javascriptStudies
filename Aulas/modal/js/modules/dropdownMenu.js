e1002e604f723a85ad4ea9ddb213fcbc192912d8
gabrielCesarino <gahnrq@hotmail.com>
2020-08-17 20:42:46 -0300

    handleOutsideClick()

--- Aulas/modal/js/modules/dropdownMenu.js
+++ Aulas/modal/js/modules/dropdownMenu.js
@@ -9,7 +9,28 @@ export default function initDropdownMenu () {
 
     function handleClick(e) {
         e.preventDefault();
-        this.classList.toggle('active');
+        this.classList.add('active');
+        outsideClick(this, () => {
+            this.classList.remove('active');
+        })
+    }
+
+    function outsideClick(element, callback) {
+        const html = document.documentElement;
+        const outside =  'data-outside'
+
+        if(!element.hasAttribute(outside)){
+            events
+            html.addEventListener('click', handleOutsideClick);
+            element.setAttribute(outside, '');
+        }
+        function handleOutsideClick (e) {
+            if(!element.contains(event.target)){
+                element.removeAttribute(outside, '');
+                callback();
+                html.removeEventListener('click', handleOutsideClick)
+            }
+        };
     }
 
 
